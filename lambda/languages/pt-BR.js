e9e1229523ae53829750350246d8db1e19e96f30
GeovanaSilva <geovanasilva.5567o@gmail.com>
2022-12-07 23:06:01 -0300

    skill desenvolvida

---
+++ lambda/languages/pt-BR.js
@@ -0,0 +1,78 @@
+module.exports = {
+  translation: {
+    SKILL_NAME: 'bebabri medi',
+    ENTRADA: [
+      'Olá. Boas vindas ao %s. Você gostaria de cadastrar um medicamento?',
+      'Oi. Boas vindas ao %s. Você gostaria de cadastrar um medicamento?',
+      'Oi. Boas vindas ao %s. Você gostaria de cadastrar um medicamento?',//
+    ],
+    ENTRADA_REPROMPT: [
+      'Você gostaria de cadastrar um medicamento?',
+      'Posso cadastrar um medicamento para você?',
+      'Eu posso cadastrar um medicamento para você. Gostaria de começar?',
+    ],
+    SCHEDULE_YES: [
+      'Ok, vamos cadastrar um medicamento.',
+      'Ok, vamos começar.',
+      'Tudo bem, vamos cadastrar um medicamento.', //
+    ],
+    SCHEDULE_NO: [
+      'Tudo bem. Volte quando você quiser cadastrar um medicamento.',
+      'Ok, eu estarei aqui a qualquer momento que você quiser cadastrar um medicamento.',
+      'Tudo bem, quando você quiser cadastrar um medicamento, retorne.',
+    ],
+    AJUDA: [
+      'Esse skill pode lhe ajudar a cadastrar um medicamento. Você gostaria de cadastrar um medicamento?',
+    ],
+    AJUDA_REPROMPT: [
+      'Você gostaria de cadastrar um medicamento?',
+    ],
+    CANCEL_STOP_RESPONSE: [
+      'Tchau',
+      'Ok, estarei aqui se precisar de mim.',
+    ],
+    FALLBACK: ['Você pode dizer: você gostaria de cadastrar um medicamento, ou que, você tem um medicamento para cadastrar. Você gostaria de cadastrar um medicamento?',
+    ],
+    FALLBACK_REPROMPT: [
+      'Você gostaria de cadastrar um medicamento?',
+    ],
+    APPOINTMENT_CONFIRM: [
+      'Você está cadastrando um medicamento com %s para %s. Posso confirmar?',
+    ],
+    APPOINTMENT_CONFIRM_REPROMPT: [
+      'Posso enviar a sua solicitação de consulta para %s?',
+    ],
+    APPOINTMENT_CONFIRM_COMPLETED: [
+      'Sua consulta com %s em %s foi agendada. Você também receberá uma confirmação via email. Por favor responda ao email de confirmação para cancelar ou reagendar. Obrigada.',
+    ],
+    TIME_AVAILABLE: [
+      '%s está disponível. Você gostaria de agendar?',
+    ],
+    TIME_AVAILABLE_REPROMPT: [
+      'Você gostaria de agendar %s?',
+    ],
+    TIME_NOT_AVAILABLE: [
+      'Ja tem um remedio nesse horario, %s não está disponível. Você gostaria de tentar outro horário?',
+    ],
+    TIME_NOT_AVAILABLE_REPROMPT: [
+      'Você gostaria de tentar outro horário?',
+    ],
+    APPOINTMENT_TITLE: 'Consulta com %s',
+    APPOINTMENT_DESCRIPTION: 'Esta é um agendamento telefônico para %s com %s. Nós ligaremos para você às %s. Se quiser cancelar or alterar essa consulta, favor responder este email.',
+    EMAIL_SUBJECT: 'Consulta para %s com %s',
+    EMAIL_TEXT: 'Esta é um agendamento telefônico para %s com %s. Nós ligaremos para você às %s. Se quiser cancelar or alterar essa consulta, favor responder este email.',
+    NO_CONFIRM: 'Ok, para reiniciar você pode dizer: agende uma consulta, ou para cancelar diga pare.',
+    NO_CONFIRM_REPROMOT: 'Você pode dizer agende uma consulta para reiniciar ou pare para cancelar.',
+    ENV_NOT_CONFIGURED: 'Uma ou mais configurações não estão ajustadas. Por favor leia o arquivo readme para ajuda.',
+    PERMISSIONS_REQUIRED: 'Para agendar e confirmar consultas, %s precisa ter acesso ao seu nome, endereço de e-mail e número de telefone. Para fornecer acesso a essas informações, por favor habilite as permissões de perfil no app da Alexa.',
+    EMAIL_REQUIRED: 'Parece que você não tem um e-mail configurado. Você pode configurar seu e-mail no app da Alexa.',
+    EMAIL_REQUIRED_REPROMPT: 'Por favor configure seu e-mail no app da Alexa.',
+    NAME_REQUIRED: 'Parece que você não configurou seu nome. Você pode configurar se nome no app da Alexa.',
+    NAME_REQUIRED_REPROMPT: 'Por favor configure seu nome no app da Alexa.',
+    PHONE_REQUIRED: 'Parece que você não configurou o número do seu celular. Você pode configurar o número do seu celular no app da Alexa.',
+    PHONE_REQUIRED_REPROMPT: 'Por favor configurer o número do seu celular no app da Alexa.',
+    ERROR: 'Desculpe, não entendi. Você poderia repetir?',
+    ERROR_REPROMPT: 'Você poderia repetir?',
+    FREEBUSY_DISABLED: 'Desculpe, a consulta de disponibilidade está desabilitada. Você gostaria de ainda assim agendar uma consulta?',
+  },
+};
